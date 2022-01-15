19186b20cc94f9bc862f00d102acb7db00c3d93d
Davide Vignozzi <davide.vignozzi@gmail.com>
2022-01-15 17:44:00 +0100

    Initial commit

---
+++ design/notes.js
@@ -0,0 +1,53 @@
+
+/*
+    const grey = "#B3B2BF";
+    const green = "#00E194";
+    const orange = "#ED8655";
+    const red = "#FB6158";
+    const black = "#363543";
+*/
+
+/*
+    let movements = [] //array con tutti i movimenti
+
+    let movement =
+    {
+        id: index
+        name: "nome"; // nome del movimento;
+        date: "date"; // data del movimento;
+        inbound: false; // è un movimento in entrata?
+        outgoing: false; // è un movimento in uscita?
+    }
+*/
+
+/*
+    function setState(){
+        if(button.inbound is pressed){
+            movement.inbound = true;
+        }
+
+        if(button.outgoing is pressed){
+            movement.outgoing = true;
+        }
+    }
+*/
+
+/*
+    // (onclick) => button aggiungi;
+
+    function pushMovement(){
+        movements.push(
+            {
+                id: index
+                name: "nome";
+                date: "date";
+                inbound: true / false;
+                outgoing: false / true;
+            }
+        )
+    }
+*/
+
+/*
+    DISPLAY MOVEMENTS ITEMS IN TABLE:
+*/
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
