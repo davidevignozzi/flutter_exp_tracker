fb4c8f20d6efd96646756c80b704c382c07ab16a
Davide Vignozzi <davide.vignozzi@gmail.com>
2022-01-25 12:00:31 +0100

    graphs works

--- lib/Widgets/Graphs/inboundGraph.dart
+++ lib/Widgets/Graphs/inboundGraph.dart
@@ -4,6 +4,7 @@ import 'package:exp_tracker/Style/palette.dart';
 import 'package:exp_tracker/gloabl.dart';
 import 'package:exp_tracker/movementList.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class inboundGraph extends StatefulWidget {
   @override
@@ -11,9 +12,13 @@ class inboundGraph extends StatefulWidget {
 }
 
 class _inboundGraphState extends State<inboundGraph> {
+  late List<GDPData> _chartData;
+
+  // to refresh every second
   @override
   var timer;
   void initState() {
+    _chartData = getChartData();
     timer = Timer.periodic(Duration(seconds: 1), (_) {
       setState(() {});
     });
@@ -28,6 +33,22 @@ class _inboundGraphState extends State<inboundGraph> {
     return Container(
         child: Column(
       children: [
+        Container(
+          width: 120,
+          height: 120,
+          child: SfCircularChart(
+            series: <CircularSeries>[
+              RadialBarSeries<GDPData, int>(
+                  innerRadius: "80%",
+                  dataSource: _chartData,
+                  pointColorMapper: (GDPData data, _) => data.color,
+                  xValueMapper: (GDPData data, _) => generalTotalOutgoings,
+                  yValueMapper: (GDPData data, _) =>
+                      generalTotalInbounds - generalTotalOutgoings,
+                  maximumValue: double.parse(generalTotalInbounds.toString())),
+            ],
+          ),
+        ),
         Text(
           generalTotalInbounds.toString(),
           style: TextStyle(
@@ -39,4 +60,17 @@ class _inboundGraphState extends State<inboundGraph> {
       ],
     ));
   }
+
+  List<GDPData> getChartData() {
+    final List<GDPData> chartData = [
+      GDPData(generalTotalInbounds, Palette.myGreen),
+    ];
+    return chartData;
+  }
+}
+
+class GDPData {
+  GDPData(this.num1, this.color);
+  final int num1;
+  final Color color;
 }
--- lib/Widgets/Graphs/outgoingsGraph.dart
+++ lib/Widgets/Graphs/outgoingsGraph.dart
@@ -4,16 +4,21 @@ import 'package:exp_tracker/Style/palette.dart';
 import 'package:exp_tracker/gloabl.dart';
 import 'package:exp_tracker/movementList.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class outgoingGraph extends StatefulWidget {
   @override
-  _outgoingGraphState createState() => _outgoingGraphState();
+  _ougoingGraphState createState() => _ougoingGraphState();
 }
 
-class _outgoingGraphState extends State<outgoingGraph> {
+class _ougoingGraphState extends State<outgoingGraph> {
+  late List<GDPData> _chartData;
+
+  // to refresh every second
   @override
   var timer;
   void initState() {
+    _chartData = getChartData();
     timer = Timer.periodic(Duration(seconds: 1), (_) {
       setState(() {});
     });
@@ -26,14 +31,45 @@ class _outgoingGraphState extends State<outgoingGraph> {
 
   Widget build(BuildContext context) {
     return Container(
-      child: Text(
-        generalTotalOutgoings.toString(),
-        style: TextStyle(
-          fontWeight: FontWeight.w800,
-          fontSize: 18,
-          color: Colors.white,
+        child: Column(
+      children: [
+        Container(
+          width: 120,
+          height: 120,
+          child: SfCircularChart(
+            series: <CircularSeries>[
+              RadialBarSeries<GDPData, int>(
+                  innerRadius: "80%",
+                  dataSource: _chartData,
+                  pointColorMapper: (GDPData data, _) => data.color,
+                  xValueMapper: (GDPData data, _) => generalTotalOutgoings,
+                  yValueMapper: (GDPData data, _) => generalTotalOutgoings,
+                  maximumValue: double.parse(generalTotalInbounds.toString())),
+            ],
+          ),
         ),
-      ),
-    );
+        Text(
+          generalTotalOutgoings.toString(),
+          style: TextStyle(
+            fontWeight: FontWeight.w800,
+            fontSize: 18,
+            color: Colors.white,
+          ),
+        ),
+      ],
+    ));
   }
+
+  List<GDPData> getChartData() {
+    final List<GDPData> chartData = [
+      GDPData(generalTotalOutgoings, Palette.myOrange),
+    ];
+    return chartData;
+  }
+}
+
+class GDPData {
+  GDPData(this.num1, this.color);
+  final int num1;
+  final Color color;
 }
